dad6d4b1d0dd3d33ef8882225189dbe0df7c14fd
iujunhyung <junehyung@iyulab.com>
2024-10-30 20:22:03 +0900

    update

--- example/ConsoleTest/Program.cs
+++ example/ConsoleTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using Raggle.Abstractions.Memory.Document;
+﻿using Raggle.Abstractions.AI;
+using Raggle.Abstractions.Memory.Document;
+using Raggle.Connector.OpenAI;
 using Raggle.Core;
 using Raggle.Core.Handlers;
 using Raggle.Core.Parsers;
@@ -10,9 +12,9 @@ using System.Text.Json;
 
 async Task<string> GetKey()
 {
-    var text = await File.ReadAllTextAsync(@"C:\data\Raggle\src\Raggle.ConsoleTests\Secrets.json");
+    var text = await File.ReadAllTextAsync(@"C:\data\Raggle\example\ConsoleTest\Secrets.json");
     var secrets = JsonSerializer.Deserialize<Dictionary<string, string>>(text);
-    var key = secrets?.GetValueOrDefault("Anthropic") ?? string.Empty;
+    var key = secrets?.GetValueOrDefault("OpenAI") ?? string.Empty;
     return key;
 }
 var documentStorage = new DiskDocumentStorage(new DiskStorageConfig
@@ -23,9 +25,20 @@ var vectorStorage = new LiteDBVectorStorage(new LiteDBConfig
 {
     DatabasePath = @"C:\temp\vector.db"
 });
+var chatService = new OpenAIChatCompletionService(await GetKey());
+var chatOption = new ChatCompletionOptions
+{ 
+    ModelId= "gpt-4o-mini",
+    MaxTokens = 2048,
+    Temperature = 0.5f,
+};
+var embeddingService = new OpenAIEmbeddingService(await GetKey());
+var embeddingOption = new EmbeddingOptions
+{
+    ModelId = "text-embedding-3-large",
+};
 
-var orchestrator = new PipelineOrchestrator(
-    documentStorage: documentStorage);
+var orchestrator = new PipelineOrchestrator(documentStorage: documentStorage);
 
 orchestrator.TryAddHandler("parse", new DocumentParsingHandler(
     documentStorage: documentStorage,
@@ -40,24 +53,40 @@ orchestrator.TryAddHandler("chunk", new TextChunkingHandler(
     documentStorage: documentStorage,
     maxTokensPerChunk: 1024));
 
+orchestrator.TryAddHandler("summarize", new GenerateSummarizedTextHandler(
+    documentStorage: documentStorage,
+    chatService: chatService,
+    chatOptions: chatOption));
+
+orchestrator.TryAddHandler("question", new GenerateQuestionHandler(
+    documentStorage: documentStorage,
+    chatService: chatService,
+    chatOptions: chatOption));
+
+orchestrator.TryAddHandler("embed", new TextEmbeddingHandler(
+    documentStorage: documentStorage,
+    vectorStorage: vectorStorage,
+    embeddingService: embeddingService,
+    embeddingOptions: embeddingOption));
+
 var memory = new MemoryService(
    documentStorage: documentStorage,
    vectorStorage: vectorStorage,
    orchestrator: orchestrator);
 
 var collection = "test";
-var file = @"C:\temp\sample\ppt_sample.pptx";
+var file = @"C:\temp\sample\pdf_sample.pdf";
 var upload = new UploadRequest
 {
     FileName = Path.GetFileName(file),
     Content = File.OpenRead(file),
 };
 
-await memory.CreateCollectionAsync(collection, 512);
+await memory.CreateCollectionAsync(collection, 3072);
 await memory.MemorizeDocumentAsync(
     collectionName: collection,
     documentId: GetDocumentId(file),
-    steps: ["parse", "chunk"],
+    steps: ["parse", "chunk", "summarize", "question", "embed"],
     uploadRequest: upload);
 
 return;
